16601d443a0c89fb912029b8e145fed2b995d9b5
manjunath-sandra <manjunath.sandra@gmail.com>
2020-06-17 22:41:58 +0530

    first real project to push into the github

---
+++ src/main/java/com/javatech/git/api/SpringBootGitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatech.git.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatech/git/api/SpringBootGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.git.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
